08e93c731163c8a3882398770b2d7c4d1a6a1b6d
Annaleah Ernst <annaleahernst@college.harvard.edu>
2017-02-04 17:29:01 -0500

    check out angle

--- navigation.py
+++ navigation.py
@@ -4,20 +4,32 @@ Author:
     Annaleah Ernst
 """
 import rospy
+import tf
 
-from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseWithCovarianceStamped, Point, Quaternion
 
 from logger import Logger
 
 class Navigation():
-    """ """
+    """ Local navigation.
+    
+    Attributes:
+        p (geometry_msgs.msg.Point): The position of the robot in the odometry frame according to
+            the robot_pose_ekf package.
+        q (geometry_msgs.msg.Quaternion): The orientation of the robot in the odometry frame
+            according the the robot_pose_ekf package.
+    """
     def __init__(self):
         self._logger = Logger("Navigation")
 
+        self.p = None
+        self.q = None
         rospy.Subscriber('/robot_pose_ekf/odom_combined', PoseWithCovarianceStamped, self._ekfCallback)
 
     def _ekfCallback(self, data):
-        self._logger.debug(data)
+        self.p = data.pose.pose.position
+        self.q = data.pose.pose.orientation
+        self._logger.debug(tf.transformations.tf.transformations.euler_from_quaternion([self.q.x, self.q.y, self.q.z, self.q.w]))
 
 if __name__ == "__main__":
     from tester import Tester
