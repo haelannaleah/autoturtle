4b54c5b8848c245443dd9b3db16ae24948f46c70
Annaleah Ernst <annaleahernst@college.harvard.edu>
2017-02-09 13:34:08 -0500

    refactoring acceleration

--- motion.py
+++ motion.py
@@ -22,8 +22,8 @@ class Motion():
     _ROT_SPEED = radians(60)
     _LIN_SPEED = 0.2
     _ACCEL_TIME = 0.1
-    _ROT_ACCEL = .025
-    _ROT_DECCEL = -.05
+    _ROT_ACCEL = .25
+    _ROT_DECCEL = -.5
     _LIN_ACCEL = .03
     _LIN_DECCEL = -.035
     _TURN_LEFT = 1
