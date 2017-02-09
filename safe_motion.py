88174ae7a4f91aff4f2c01309dc294285895cc6b
Annaleah Ernst <annaleahernst@college.harvard.edu>
2017-02-09 15:20:25 -0500

    found error

--- safe_motion.py
+++ safe_motion.py
@@ -21,19 +21,19 @@ class SafeMotion(Motion):
         """ Both be safe and premptive. """
         # if we see a cliff or get picked up, stop
         if self.sensors.cliff or self.sensors.wheeldrop:
-            Motion.stop(self, now=True)
+            self.stop(self, now=True)
     
         # if we hit something, stop
         elif self.sensors.bump:
             if self.walking:
-                Motion.stop_linear(self, now=True)
+                self.stop_linear(self, now=True)
             else:
                 self.avoiding = True
-                Motion.turn(self, self.sensors.bumper > 0)
+                self.turn(self, self.sensors.bumper > 0)
             
         elif self.avoiding:
             if self.walking or self.turning:
-                Motion.stop(self)
+                self.stop(self)
             else:
                 self.avoiding = False
         
@@ -45,11 +45,11 @@ class SafeMotion(Motion):
         """ The generic safety wrapper for the Turtlebot. """
         # if we see a cliff or get picked up, stop
         if self.sensors.cliff or self.sensors.wheeldrop:
-            Motion.stop(self, now=True)
+            self.stop(self, now=True)
 
         # if we hit something, stop
         elif self.sensors.bump:
-            Motion.stop_linear(self, now=True)
+            self.stop_linear(self, now=True)
         
         # otherwise, we keep going
         else:
@@ -69,6 +69,9 @@ class SafeMotion(Motion):
 
     def rotational_stop(self, now=False):
         self._safetyStop(Motion.rotational_stop, now)
+
+    def shutdown(self, rate):
+        self._safetyStop(Motion.shutdown, rate)
 
 if __name__ == "__main__":
     from tester import Tester
