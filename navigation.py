b75d51c44f995afb28e68e071b4fa91474f5d60f
Annaleah Ernst <annaleahernst@college.harvard.edu>
2017-02-10 16:59:12 -0500

    test line

--- navigation.py
+++ navigation.py
@@ -104,9 +104,9 @@ if __name__ == "__main__":
 
         def main(self):
             """ The test currently being run. """
-            self.testCCsquare(.5)
+            #self.testCCsquare(.5)
             # self.testCsquare(.5)
-            # self.testLine(1)
+            self.testLine(1)
         
         def gotToPos(self, name, x, y):
             """ Default behavior for testing goToPosition. 
