755b4d11a9b3b0aadd4ced3abf7d0b626d177835
Annaleah Ernst <annaleahernst@college.harvard.edu>
2017-02-23 12:56:05 -0500

    syntax

--- logger.py
+++ logger.py
@@ -85,15 +85,15 @@ class Logger:
         # if we haven't been writing to this already, open it up
         if filename not in self._open_files:
             self._open_files[filename] = {}
-            self._open_files[filename].file = open(filename + datetime.now().strftime("_%Y%m%d-%H%M%S") + ".csv", "w+")
-            self._open_files[filename].writer = csv.writer(self._open_files[filename].file)
+            self._open_files[filename]["file"] = open(filename + datetime.now().strftime("_%Y%m%d-%H%M%S") + ".csv", "w+")
+            self._open_files[filename]["writer"] = csv.writer(self._open_files[filename].file)
 
-        self._open_files[filename].writer.writerow(msg)
+        self._open_files[filename]["writer"].writerow(msg)
 
     def shutdown(self):
         """ Close any open logging files. """
         for filename in self._open_files:
-            self.open_file[filename].file.close()
+            self.open_file[filename]["file"].close()
 
 if __name__ == "__main__":
     from tester import Tester
