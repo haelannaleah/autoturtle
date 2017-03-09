10054c62dfdf0a3cb2cd6b0940fb0f43b99ec947
Annaleah Ernst <annaleahernst@college.harvard.edu>
2017-03-09 15:12:14 -0500

    test line

--- navloc.py
+++ navloc.py
@@ -113,9 +113,9 @@ if __name__ == "__main__":
 
         def main(self):
             """ The test currently being run. """
-            self.testCCsquare(.5)
+            #self.testCCsquare(.5)
             #self.testCsquare(.5)
-            #self.testLine(1)
+            self.testLine(1)
             
         
         def initFile(self, filename):
