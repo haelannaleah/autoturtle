a28d44444f2e2cedbd2ff5b4ce5a415e25173b82
Annaleah Ernst <annaleahernst@college.harvard.edu>
2017-03-09 13:00:29 -0500

    skeleton code for navloc complete

--- navloc.py
+++ navloc.py
@@ -5,10 +5,10 @@ Author:
 """
 import tf
 import rospy
-import threading
 
 from copy import deepcopy
 from geometry_msgs.msg import Pose, Point, Quaternion
+from math import sin, cos
 
 from navigation import Navigation
 from localization import Localization
@@ -17,35 +17,15 @@ from logger import Logger
 class NavLoc(Navigation, Localization):
     def __init__(self, landmarks):
         
-        # create transformation and broadcaster
-        self._broadcaster = tf.TransformBroadcaster()
+        # create transformation object
         self._transform = {"position": Point(0,0,0), "angle" = 0}
-        self._lock = threading.lock()
-        self._tf_thread = threading.Thread(target=self._broadcastTransform)
-        self._tf_thread.start()
     
         # initialize what we're inheriting from
         Navigation.__init__(self)
         Localization.__init__(self, landmarks)
-        self._raw_pose = Pose
+        self._raw_pose = Pose()
 
         self._logger = Logger("NavLoc")
-
-    def _broadcastTransform(self):
-        """ Broadcast the current transformation. """
-        
-        # set the broadcast rate
-        rate = rospy.Rate(10)
-        
-        # as long as the robot is going, broadcast the transformation
-        while not rospy.is_shutdown:
-            # TODO: determine if locking is necessary
-            self._lock.acquire()
-            self._broadcaster(self._transform["position"], self._transform["orientation"],
-                rospy.Time.now(), "/robot_pose_ekf", "/map")
-            self._lock.release()
-            
-            rate.sleep()
     
     def _estimatePose(self):
         """ Override pose estimation to include pose calculation. """
@@ -60,17 +40,26 @@ class NavLoc(Navigation, Localization):
         ekf_q = ekf_pose.orientation
         ekf_angle = tf.transformations.euler_from_quaternion([ekf_q.x, ekf_q.y, ekf_q.z, ekf_q.w])[-1]
         
-        self.angle = self.estimated_pose.
+        self._transform["angle"] = self.estimated_angle - ekf_angle
+        self._transform["position"].x = self.estimated_pose.x - ekf_pose.position.x
+        self._transform["position"].y = self.estimated_pose.y - ekf_pose.position.y
 
     def _ekfCallback(self, data):
         """ Process robot_pose_ekf data. """
         self._raw_pose = data.pose.pose
+        p = self._raw_pose.position
+        q = self._raw_pose.orientation
 
-        # transform to the map frame
-        self.p = None
-        self.q = None
+        # transform from odom to the map frame
+        self.p = Point()
+        self.p.x = self._transform["position"].x + p.x * cos(self._transform["angle"]) + p.y * sin(self._transform["angle"])
+        self.p.y = self._transform["position"].y - p.x * sin(self._transform["angle"]) + p.y * cos(self._transform["angle"])
         
         # since a quaternion respresents 3d space, and turtlebot motion is in 2d, we can just
         #   extract the only non zero euler angle as the angle of rotation in the floor plane
-        self.angle = tf.transformations.euler_from_quaternion([self.q.x, self.q.y, self.q.z, self.q.w])[-1]
+        self.angle = tf.transformations.euler_from_quaternion([q.x, q.y, q.z, q.w])[-1]
+        self.angle += self.transform["angle"]
+
+        # we're deciding not to care about the quaternion for now
+        self.q = None
 
