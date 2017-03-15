246a638ffb088aed720c013022e2dc5ee7b99aa9
Annaleah Ernst <annaleahernst@college.harvard.edu>
2017-03-15 13:08:34 -0400

    remove time

--- localization.py
+++ localization.py
@@ -141,8 +141,8 @@ class Localization():
         y = map.pose.position.y - r * sin(theta)
         
         # make sure that we aren't getting insane localization data and are sampling at our reduced rate
-        if (np.allclose([x, y, delta], self._prev_est, atol = 0.1, rtol = 0.05)
-            and self._prev_update_time - time() > self._AR_UPDATE_TIME):
+        if (np.allclose([x, y, delta], self._prev_est, atol = 0.1, rtol = 0.05)):
+            #and self._prev_update_time - time() > self._AR_UPDATE_TIME):
             
             # plug this into an estimated pose in the map frame
             q = tf.transformations.quaternion_from_euler(0,0,delta)
