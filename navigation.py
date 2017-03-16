087d9fc7355290e4db661bae6ba859635ff93dfa
Annaleah Ernst <annaleahernst@college.harvard.edu>
2017-03-16 18:13:28 -0400

    set avoid time

--- navigation.py
+++ navigation.py
@@ -45,7 +45,7 @@ class Navigation(Motion):
     _MIN_LINEAR_SPEED = .25
     
     # set avoidance time
-    _AVOID_TIME = .1
+    _AVOID_TIME = .25
     
     def __init__(self, jerky = False, walking_speed = 1):
     
