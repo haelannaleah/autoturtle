1306009d87b21e793916b0d28d1137def6db6a21
Annaleah Ernst <annaleahernst@college.harvard.edu>
2017-03-16 18:19:15 -0400

    fix the turning issue

--- navigation.py
+++ navigation.py
@@ -155,6 +155,8 @@ class Navigation(Motion):
 
         # if we're in avoidance mode, just go forward
         elif time() - self._avoid_time < self._AVOID_TIME:
+            while self._motion.starting:
+                self._avoid_time = time()
             self._motion.stopRotation(now = self._jerky)
             self._motion.walk(speed = self._walking_speed)
             self._logger.debug("avoiding")
