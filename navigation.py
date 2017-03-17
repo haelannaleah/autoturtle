b21e01fd6051769f4e4f90c9872c315dd8c99507
Annaleah Ernst <annaleahernst@college.harvard.edu>
2017-03-16 20:33:38 -0400

    turn and walk

--- navigation.py
+++ navigation.py
@@ -179,10 +179,13 @@ class Navigation(Motion):
             self._avoid_time = time()
             
         # if there's a wall, we need to get around it
-        elif self._sensors.wall and self._avoiding:
+        elif self._avoiding:
         
             self._logger.debug("in wall")
-            self._motion.turn(self._sensors.wall_dir > 0, speed = self._MIN_MOVING_TURN_SPEED)
+            
+            if self._sensors.wall:
+                self._motion.turn(self._sensors.wall_dir > 0, speed = self._MIN_MOVING_TURN_SPEED)
+            
             self._motion.walk(speed = self._walking_speed)
 
         else:
