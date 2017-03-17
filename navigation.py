61d0ddde18560835106d1139081a63995d4bd082
Annaleah Ernst <annaleahernst@college.harvard.edu>
2017-03-16 20:50:20 -0400

    try a thing

--- navigation.py
+++ navigation.py
@@ -181,7 +181,10 @@ class Navigation(Motion):
         # if there's a wall, we need to get around it
         elif self._avoiding:
             if self._sensors.wall:
-                self._motion.turn(self._sensors.wall_dir > 0, speed = self._MAX_MOVING_TURN)
+                if (nav_val <= 0) != (self._sensors.wall_dir > 0):
+                    self._motion.turn(self._sensors.wall_dir > 0, speed = self._MAX_MOVING_TURN)
+                else:
+                    self._avoid_time = 0
             else:
                 self._motion.stopRotation()
             
